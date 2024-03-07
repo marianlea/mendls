7b0c92f396bf022cc740e60a575ab3645ca16bc6
Lea Baldevieso <mlbaldevieso@gmail.com>
2024-03-07 23:01:06 +1100

    restructured client folder

--- mendls-client/src/App.jsx
+++ mendls-client/src/App.jsx
@@ -1,34 +1,131 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import PastriesList from './pages/PastriesList'
+import BasketList from './pages/BasketList'
+import Pastries from './utils/mendls_api'
+import NavBar from './components/NavBar'
+import Homepage from './pages/Homepage'
+import About from './pages/About'
+import Drawer from '@mui/material/Drawer';
+import noise from './assets/noise.png';
+import { useLocation } from 'react-router-dom'
+import PaymentSuccess from './pages/PaymentSuccess'
+import PaymentCancel from './pages/PaymentCancel'
+import Checkout from './pages/Checkout'
+import ContactPage from './pages/ContactPage'
+import Pastry from './pages/Pastry'
+
+const drawerWidth = 650;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [pastries, setPastries] = useState([])
+  const savedBasket = JSON.parse(localStorage.getItem('basket'))
+  const [basket, setBasket] = useState(savedBasket || [])
+  const [isBasketVisible, setIsBasketVisible] = useState(false)
+  const location = useLocation()
+
+  
+  useEffect(()=>{
+    Pastries.all()
+      .then(pastries => {
+        setPastries(pastries)
+      })
+  }, [])
+    
+  useEffect(() => {
+    localStorage.setItem('basket', JSON.stringify(basket))
+  }, [basket])
+
+  function addToBasket(pastry) {
+    const existingPastryIndex = basket.findIndex(item => item.id === pastry.id);
+  
+    if (existingPastryIndex !== -1) {
+      const updatedBasket = basket.map((item, index) => {
+        if (index === existingPastryIndex) {
+          return {...item, ...pastry};
+        }
+        return item;
+      });
+      setBasket(updatedBasket);
+    } else {
+      setBasket([...basket, pastry]);
+    }
+  }
+
+  function removeItemFromBasket(pastry) {
+    setBasket((prev) => {
+      return prev.filter(item => pastry.id !== item.id )
+    })
+    clearInputValue(pastry.id)
+  }
+
+  function clearInputValue(id) {
+    setPastries((prev) => {
+      const updatedPastries = prev.map(pastry => {
+        if (pastry.id === id) {
+          return { ...pastry, qunatity: null }
+        }
+        return pastry
+      })
+      return updatedPastries
+    })
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <NavBar 
+        setIsBasketVisible={setIsBasketVisible} 
+        basket={basket}
+        location ={location} 
+      />
+      <Routes>
+        <Route path='/' element={
+          <>
+            <Homepage />
+            <PastriesList 
+              pastries={pastries} 
+              basket={basket} 
+              onBasketChange={addToBasket}
+              footerVisible={location.pathname === '/#shop'} />
+          </>
+        } />
+        <Route path='/about' element={<About />} />
+        <Route path='/success' element={<PaymentSuccess resetBasket={setBasket} />} />
+        <Route path='/cancel' element={<PaymentCancel />} />
+        <Route path='/checkout' element={<Checkout basket={basket} />} />
+        <Route path='/contact' element={<ContactPage />} />
+        {pastries.map(pastry => {
+          return <Route 
+                  path={`/shop/${pastry.title}`} 
+                  element={<Pastry pastry={pastry}
+                  basket={basket} 
+                  onBasketChange={addToBasket}/>} 
+                 />})}
+      </Routes>
+      <Drawer 
+        open={isBasketVisible} 
+        anchor="right"
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+            flexShrink: 0,
+            backgroundImage: `url(${noise})`,
+            backgroundPosition: 'center',
+            backgroundColor: '#FFFAF0'
+          },
+        }}
+      >
+        <BasketList 
+          basket={basket} 
+          onBasketChange={addToBasket} 
+          setIsBasketVisible={setIsBasketVisible} 
+          removeItemFromBasket={removeItemFromBasket}/>
+      </Drawer>
+    </div>
   )
 }
 
