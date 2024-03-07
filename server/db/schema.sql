3e4e928bee03ce76c605e0c8baca212e2df0e5a1
Lea Baldevieso <mlbaldevieso@gmail.com>
2024-03-07 22:53:37 +1100

    restructured client folder

--- server/db/schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE mendls_api;
-
-CREATE TABLE pastries (
-    id SERIAL PRIMARY KEY,
-    title TEXT NOT NULL,
-    description TEXT NOT NULL,
-    price NUMERIC(7, 2),
-    unit INTEGER
-);
