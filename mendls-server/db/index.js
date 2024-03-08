a898801efc5ba58d802f1796800a25dfe9ec7436
Lea Baldevieso <mlbaldevieso@gmail.com>
2024-03-08 14:13:56 +1100

    vercel update

--- mendls-server/db/index.js
+++ mendls-server/db/index.js
@@ -2,21 +2,12 @@ const { Pool } = require('pg')
 const fs = require('fs')
 require('dotenv').config()
 
-const seedQuery = fs.readFileSync('db/seed', 'utf-8')
-
 if (!process.env.POSTGRES_URL) {
     throw Error('database url missing')
 }
 
 const pool = new Pool({
   connectionString: process.env.POSTGRES_URL,
-})
-
-pool.query(seedQuery, err => {
-  if (err) {
-    throw err
-  }
-  console.log('SQL seed completed')
 })
 
   
