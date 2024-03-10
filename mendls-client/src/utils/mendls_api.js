bf7e44f6bccd7365b1fa8b8b5be8aaba886f2d7c
Lea Baldevieso <mlbaldevieso@gmail.com>
2024-03-10 15:00:13 +1100

    vercel update

--- mendls-client/src/utils/mendls_api.js
+++ mendls-client/src/utils/mendls_api.js
@@ -1,10 +1,10 @@
-require('dotenv').config()
 import axios from "axios";
+const SERVER_URL = process.env.REACT_APP_SERVER_URL
 
 
 
 async function all() {
-    let res = await axios.get(`${process.env.SERVER_URL}/api/pastries`)
+    let res = await axios.get(`${SERVER_URL}/api/pastries`)
     return res.data
 }
 
