5277efc065dbf659c8b65b24b77ac3280aa03020
Lea Baldevieso <mlbaldevieso@gmail.com>
2024-03-10 14:14:14 +1100

    vercel update

--- mendls-client/vite.config.js
+++ mendls-client/vite.config.js
@@ -4,10 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   proxy: {
-  //     '/api': 'https://mendls-server.vercel.app',
-  //   },
-  //   port: '3006'
-  // }
+  server: {
+    proxy: {
+      '/api': 'https://mendls-server.vercel.app',
+    }
+  }
 })
